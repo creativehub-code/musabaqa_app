29336e8cfa3fc20ccaca89f472ebb9f8ead4703f
SoloSix <mdbilalpulthara@duck.com>
2026-01-31 23:54:07 +0530

    feat: Initialize backend server with Express, database connection, and API routes.

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin:[
+    "http://localhost:3000",
+    "musabaqa-app-zjxk.vercel.app"
+  ],
+  credentials: true
+}));
 app.use(express.json({ limit: "10mb" }));
 
 // Routes
